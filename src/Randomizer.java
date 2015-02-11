8306c2997d9e4ff04d6b25c801eb49deca842b04
Patrick Karjala <pkarjala@hawaii.edu>
2015-02-11 13:59:28 -1000

    Finished with comment fixes.

--- src/Randomizer.java
+++ src/Randomizer.java
@@ -1,54 +1,83 @@
-// This example is from _Java Examples in a Nutshell_. (http://www.oreilly.com)
-// Copyright (c) 1997 by David Flanagan
-// This example is provided WITHOUT ANY WARRANTY either expressed or implied.
-// You may study, use, modify, and distribute it for non-commercial purposes.
-// For any commercial use, see http://www.davidflanagan.com/javaexamples
-
 /**
- * This class defines methods for computing pseudo-random numbers, and it defines
- * the state variable that needs to be maintained for use by those methods.
- **/
+ * This example is from _Java Examples in a Nutshell_. (http://www.oreilly.com)
+ * Copyright (c) 1997 by David Flanagan
+ * This example is provided WITHOUT ANY WARRANTY either expressed or implied.
+ * You may study, use, modify, and distribute it for non-commercial purposes.
+ * For any commercial use, see http://www.davidflanagan.com/javaexamples
+ * This class defines methods for computing pseudo-random numbers, and it
+ * defines the state variable that needs to be maintained for use by those
+ * methods.
+ */
 public class Randomizer {
-    // Carefully chosen constants from the book "Numerical Recipes in C".
-    // All "static final" fields are constants.
-    static final int m = 233280;  // the "modulus"
-    static final int a = 9301;    // the "multiplier"
-    static final int c = 49297;   // the "increment"
+  /**
+   * The Modulus for the random number generator.
+   */
+  private static final int MODULUS = 233280;
+  /**
+   * The Multiplier for the random number generator.
+   */
+  private static final int MULTIPLIER = 9301;
+  /**
+   * The increment for the random number generator.
+   */
+  private static final int INCREMENT = 49297;
 
-    // The state variable maintained by each Randomizer instance
-    long seed = 1;
+  /**
+   * The state variable maintained by each Randomizer instance.
+   */
+  private long seed = 1;
 
   /**
    * The constructor for the Randomizer() class.  It must be passed some
    * arbitrary initial value or "seed" for its pseudo-randomness.
-   **/
-  public Randomizer(long seed) { this.seed = seed; }
+   *
+   * @param localSeed The seed value for the random number generator.
+   */
+  public Randomizer(final long localSeed) {
+    this.seed = localSeed;
+  }
 
   /**
    * This method computes a pseudo-random number between 0 and 1 using a very
    * simple algorithm.  Math.random() and java.util.Random are actually a lot
    * better at computing randomness.
-   **/
-  public float randomFloat() {
-      seed = (seed * a + c) % m; return (float)seed/(float)m;
+   *
+   * @return Returns a psuedo-random random number between 0 and 1.
+   */
+  public final float randomFloat() {
+    seed = ((seed * MULTIPLIER + INCREMENT) % MODULUS);
+    return ((float) seed / (float) MODULUS);
   }
 
   /**
    * This method computes a pseudo-random integer between 0 and specified
    * maximum.  It uses randomFloat() above.
-   **/
-  public int randomInt(int max) {
-      return Math.round(max * randomFloat());
+   *
+   * @param max The specified maximum number
+   * @return A psuedo-random number between 0 and max
+   */
+  public final int randomInt(final int max) {
+    return (Math.round(max * randomFloat()));
   }
 
   /**
    * This nested class is a simple test program: it prints 10 random integers.
    * Note how the Randomizer object is seeded using the current time.
-   **/
+   */
   public static class Test {
-      public static void main(String[] args) {
-        Randomizer r = new Randomizer(new java.util.Date().getTime());
-        for(int i = 0; i < 10; i++) System.out.println(r.randomInt(100));
+
+    /**
+     * The method for starting our random number generator.
+     *
+     * @param args ignored.
+     */
+    public static void main(final String[] args) {
+      final int loop = 10;
+      final int randomSeed = 100;
+      Randomizer randomNumber = new Randomizer(new java.util.Date().getTime());
+      for (int i = 0; i < loop; i++) {
+        System.out.println(randomNumber.randomInt(randomSeed));
       }
+    }
   }
 }
---
+++ src/package-info.java
@@ -0,0 +1,3 @@
+/**
+ * Created by patrick on 2/11/15.
+ */
